f46ab7064bb5caf35326b80b5228cf6cc76ce925
PC13 <PC13@INFO5-5384>
2023-03-16 08:29:30 +0100

    feat: aggiunta metodo CalcolaParabola

--- AS2223_4G_INF_PieriEdoardo_API/Controllers/MathController.cs
+++ AS2223_4G_INF_PieriEdoardo_API/Controllers/MathController.cs
@@ -40,5 +40,39 @@ namespace AS2223_4G_INF_PieriEdoardo_API.Controllers
             status = "OK";
             return Json(new {output, status});
         }
+        [HttpGet("CalcolaParabola")]
+        public JsonResult CalcolaParabola(int a, int b, int c)
+        {
+            double sol1, sol2;
+            string status, tipoParabola;
+            int nSoluzioni;
+            int d = (b * b) - 4 * (a * c);
+            
+            if(d < 0)   //se l equazione e impossibile
+            {
+                sol1 = 0;
+                sol2 = 0;
+                nSoluzioni = 0;
+                status = "OK";
+                tipoParabola = "";
+            } else if(d >= 0)   //se ha una o due soluzioni (nSoluzioni indica il numero di soluzioni)
+            {
+                sol1 = ((b * -1) + Math.Sqrt(d)) / (a * 2);
+                sol2 = ((b * -1) - Math.Sqrt(d)) / (a * 2);
+                if (sol1 == sol2) nSoluzioni = 1;
+                else nSoluzioni = 2;
+                if (a > 0) tipoParabola = "convessa";
+                else tipoParabola = "concava";
+                status = "OK";
+            } else
+            {
+                sol1 = 0;
+                sol2 = 0;
+                status = "KO";
+                tipoParabola = "";
+                nSoluzioni = 0;
+            }
+            return Json(new { sol1, sol2, status, tipoParabola, a, b, c, nSoluzioni});
+        }
     }
 }
