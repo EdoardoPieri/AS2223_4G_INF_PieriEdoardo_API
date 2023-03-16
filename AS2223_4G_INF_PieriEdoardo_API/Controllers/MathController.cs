0c1154036b129fdd5415cb427b1915c632514737
PC13 <PC13@INFO5-5384>
2023-03-16 08:12:10 +0100

    feat: aggiunta metodo pari o dispari

--- AS2223_4G_INF_PieriEdoardo_API/Controllers/MathController.cs
+++ AS2223_4G_INF_PieriEdoardo_API/Controllers/MathController.cs
@@ -2,8 +2,31 @@
 
 namespace AS2223_4G_INF_PieriEdoardo_API.Controllers
 {
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/v1/")]
     public class MathController : Controller
     {
-
+        [HttpGet("PariODispari")]
+        public JsonResult PariODispari(int num)
+        {
+            string output, status;
+            if (num % 2 == 0)
+            {
+                output = "pari";
+                status = "OK";
+            }
+            else if (num % 2 != 0)
+            {
+                output = "dispari";
+                status = "OK";
+            }
+            else
+            {
+                output = "";
+                status = "KO";
+            }
+            return Json(new {output, status });
+        }
     }
 }
